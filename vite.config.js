f8544589547a640cfae90a9ff24958223e521ee2
Daniel Gbesimowo <danielgbesimowo7@gmail.com>
2025-12-31 05:58:31 +0100

    fix base path case issue

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: process.env.NODE_ENV === "production"?'/FocusGrid/' : "/",
+  base: process.env.NODE_ENV === "production"
+  ? "/focus-grid/"
+  : "/",
+
 })
